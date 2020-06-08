72f68f298dc6ea48aeb072d996575a08a316f61b
Tommy Chen <32497892+nac018@users.noreply.github.com>
2020-06-08 11:20:48 -0700

    Update BitInputStream.cpp

--- BitInputStream.cpp
+++ BitInputStream.cpp
@@ -1,13 +1,3 @@
-/* Authors: Nang Chen (cs100fao)
- *          Haonan Huang (cs100fbd)		 
- * Date: 11/05/2017
- * Description: This class implements the way to read one bit/byte 
- * 		from the buffer and also one byte from the input 
- * 		stream to fill the bitwise buffer.
- * Assignment: PA3
- * File: BitInputStream.cpp
- */
-
 #include "BitInputStream.h"
 
 /** Fill the bitwise buffer by reading one byte from the input stream.
